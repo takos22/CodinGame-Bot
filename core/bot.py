d77e0db79a127d04c2dc3d99c29f838d36cf8436
Takos22 <tora.akos@gmail.com>
2020-09-30 22:11:20 +0200

    Add on_command_error

--- core/bot.py
+++ core/bot.py
@@ -1,5 +1,20 @@
 import discord
 from discord.ext import commands
+from discord.errors import (
+    CheckFailure,
+    BadUnionArgument,
+    CommandOnCooldown,
+    PrivateMessageOnly,
+    NoPrivateMessage,
+    MissingRequiredArgument,
+    ConversionError,
+    BotMissingPermissions,
+    MissingPermissions,
+    BotMissingAnyRole,
+    BotMissingRole,
+    MissingRole,
+    MissingAnyRole,
+)
 
 import datetime
 import os
@@ -45,6 +60,67 @@ class Bot(commands.Bot):
 
     async def on_disconnect(self):
         print("Successfully logged out")
+
+    async def on_command_error(self, ctx, exception):
+        await self.wait_until_ready()
+
+        error = getattr(exception, "original", exception)
+
+        if hasattr(ctx.command, "on_error"):
+            return
+
+        elif isinstance(error, CheckFailure):
+            return
+
+        if isinstance(
+            error,
+            (
+                BadUnionArgument,
+                CommandOnCooldown,
+                PrivateMessageOnly,
+                NoPrivateMessage,
+                MissingRequiredArgument,
+                ConversionError,
+            ),
+        ):
+            return await ctx.send(str(error))
+
+        elif isinstance(error, BotMissingPermissions):
+            return await ctx.send(
+                "I am missing these permissions to do this command:" f"\n{self.lts(error.missing_perms)}"
+            )
+
+        elif isinstance(error, MissingPermissions):
+            return await ctx.send(
+                "You are missing these permissions to do this command:" f"\n{self.lts(error.missing_perms)}"
+            )
+
+        elif isinstance(error, (BotMissingAnyRole, BotMissingRole)):
+            return await ctx.send(
+                f"I am missing these roles to do this command:"
+                f"\n{self.lts(error.missing_roles or [error.missing_role])}"
+            )
+
+        elif isinstance(error, (MissingRole, MissingAnyRole)):
+            return await ctx.send(
+                f"You are missing these roles to do this command:"
+                f"\n{self.lts(error.missing_roles or [error.missing_role])}"
+            )
+
+        else:
+            error_embed = discord.Embed(
+                title="Error you didn't think of",
+                description=f"{self.context.author} raised this error that you didnt think of.",
+                colour=0xFF0000,
+                timestamp=datetime.datetime.utcnow(),
+            )
+            error_embed.set_author(name="send_command_help")
+            error_embed.add_field(name="Type", value=type(error).__name__)
+            error_embed.add_field(name="Error", value=str(error))
+            error_embed.add_field(name="Channel", value=self.context.channel.mention)
+            error_embed.add_field(name="Message", value=f"[{self.context.message.id}]({self.context.message.jump_url})")
+            await self.get_user(self.owner_id).send(embed=error_embed)
+            raise error
 
     @staticmethod
     def embed(ctx, *, title, description, color=0xFCD207) -> discord.Embed:
