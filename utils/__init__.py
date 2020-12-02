91623335e79ef7b6ca0f626da575c0a1454404ff
Takos22 <takos2210@gmail.com>
2020-12-02 13:14:38 +0100

    Add utils

---
+++ utils/__init__.py
@@ -0,0 +1,1 @@
+from .text import indent, dedent, shorten, color
---
+++ utils/text.py
@@ -0,0 +1,38 @@
+import textwrap
+from textwrap import shorten
+
+# ---------------------------------------------------------------------------------------------
+# Formatting
+
+
+def indent(text: str, amount: int, char: str = " ") -> str:
+    """Indent every line of `text` by `amount` of `char` (default: `" "`)"""
+    return textwrap.indent(text, amount * char)
+
+
+def dedent(text: str, keepends: bool = False) -> str:
+    "Removes the indent at the start of every line"
+    return "".join([line.lstrip() for line in text.splitlines(keepends)])
+
+
+# ---------------------------------------------------------------------------------------------
+# Colors
+
+
+class Colors:
+    BLUE = "\033[94m"
+    CYAN = "\033[96m"
+    GREEN = "\033[92m"
+    PURPLE = "\033[95m"
+    RED = "\033[91m"
+    YELLOW = "\033[93m"
+    WHITE = "\033[1m"
+    UNDERLINE = "\033[4m"
+    END = "\033[0m"
+
+
+def color(text: str, color: str) -> str:
+    color = getattr(Colors, color.upper(), "")
+    if color:
+        text = f"{color}{text}{Colors.END}"
+    return text
