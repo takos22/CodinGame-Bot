324ff1e5f80b095a3096fcc1712077d3eb3ef1aa
Takos22 <takos2210@gmail.com>
2020-12-02 13:14:45 +0100

    Update config

--- config.py
+++ config.py
@@ -1,7 +1,20 @@
 import os
 from dotenv import load_dotenv
 
+
 class Config:
     load_dotenv()
 
     TOKEN = os.environ.get("TOKEN")
+
+    DEFAULT_COGS = [
+        "jishaku",
+        "cogs._help",
+        "cogs.commands",
+        "cogs.codingame",
+        "cogs.log",
+    ]
+
+    OWNER = 401346079733317634
+    GUILD = 754028526079836251
+    SERVER_LOG_CHANNEL = 754240215056384001
